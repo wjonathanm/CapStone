48fdb5832e6e1f6f0f7ac3449d2c29e4757dd673
jakedudziak <55498677+jakedudziak@users.noreply.github.com>
2023-02-24 01:49:23 -0600

    finished everything except graphs and request form

--- cap_proj/src/Employee/ePTO.js
+++ cap_proj/src/Employee/ePTO.js
@@ -65,7 +65,25 @@ const EmployeePTO = () => {
                     </div> */}
                 </div>
                     
-                <h1 className="emp-header">Pending Requests</h1>
+                <div className="emp-header">
+                    <h1>Pending Requests</h1>
+                    <br></br>
+                    <table className="e-pending-requests">
+                        <tr>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                            <th>PTO Type</th>
+                            <th>Request Date</th>
+                        </tr>
+                        <tr>
+                            <td>3/20/2023</td>
+                            <td>3/20/2023</td>
+                            <td>Vacation</td>
+                            <td>2/25/2023</td>
+
+                        </tr>
+                    </table>
+                </div>
                 <div className="emp-header">
                     <h1 >Holidays</h1>
                     <div className="holiday-table">
