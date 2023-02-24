99d659237b5f975d890802ebdd630955eff318c6
jakedudziak <55498677+jakedudziak@users.noreply.github.com>
2023-02-24 01:08:29 -0600

    Started adding CS

--- cap_proj/src/Employee/ePTO.js
+++ cap_proj/src/Employee/ePTO.js
@@ -4,7 +4,7 @@ import Navbar from "./eNavbar";
 // import DoughnutChart from "../Components/DoughnutChart";
 
 const EmployeePTO = () => {
-    // const [userData, setUserData] = useState({
+    // const [ userData, setUserData ] = useState({
     //     labels: UserData.map((data) => data.year),
     //     datasets: [
     //       {
@@ -24,34 +24,63 @@ const EmployeePTO = () => {
     return ( 
         <div>
             <Navbar />
-            {/* <br />
             <br />
-            <h1 className="header-graph">PTO Balance</h1>
-            <div className="donut-graphs">
-                <div style={{ width: 200 }}>
-                    Personal
-                    <DoughnutChart chartData={userData} />
+            {/* <br /> */}
+            <div className="grid-epto-container">
+                <h1 className="emp-header">PTO Balance</h1>
+                {/* <div className="donut-graphs">
+                    <div style={{ width: 200 }}>
+                        Personal
+                        <DoughnutChart chartData={userData} />
+                    </div>
+                    <div style={{ width: 200 }}>
+                        Sick
+                        <DoughnutChart chartData={userData} />
+                    </div>
+                    <div style={{ width: 200 }}>
+                        Vacation
+                        <DoughnutChart chartData={userData} />
+                    </div>
+                    <div style={{ width: 200 }}>
+                        Total
+                        <DoughnutChart chartData={userData} />
+                    </div>
+                </div> */}
+                <div className="emp-header">
+                    <h1>Upcoming Days Off</h1>
+                    <div className="days-off">
+                        <div className="days-off-contents">Start Date</div>
+                        <div className="days-off-contents">End Date</div>
+                        <div className="days-off-contents">Type</div>
+                    </div>
+                    <div className="days-off">
+                        <div className="days-off-contents">2/24/2023</div>
+                        <div className="days-off-contents">2/25/2023</div>
+                        <div className="days-off-contents">Sick</div>
+                    </div>
+                    {/* <div className="days-off">
+                        <div className="days-off-contents">2/29/2023</div>
+                        <div className="days-off-contents">2/30/2023</div>
+                        <div className="days-off-contents">Vacation</div>
+                    </div> */}
                 </div>
-                <div style={{ width: 200 }}>
-                    Sick
-                    <DoughnutChart chartData={userData} />
-                </div>
-                <div style={{ width: 200 }}>
-                    Vacation
-                    <DoughnutChart chartData={userData} />
-                </div>
-                <div style={{ width: 200 }}>
-                    Total
-                    <DoughnutChart chartData={userData} />
+                    
+                <h1 className="emp-header">Pending Requests</h1>
+                <div className="emp-header">
+                    <h1 >Holidays</h1>
+                    <div className="holiday-table">
+                        <br/>
+                        <div className="holidays">Christmas</div>
+                        <div className="holidays">12/15/2023</div>
+                    </div>
+                    <div className="holiday-table">
+                        <br/>
+                        <div className="holidays">Holdiay Name</div>
+                        <div className="holidays">Holdiay Date</div>
+                    </div>
+                    
                 </div>
             </div>
-            <br />
-            <h1 className="header-graph">Upcoming Days Off</h1>
-            <div>
-            </div>
-            <br />
-            <h1 className="header-graph">Pending Requests</h1>
-            <h1 className="header-graph">Holidays</h1> */}
         </div>
      );
 }
