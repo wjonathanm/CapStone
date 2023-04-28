91f7ba984df80018f4afe1e45d06c3da7c53c727
zombieninja80 <113934878+wjonathanm@users.noreply.github.com>
2023-04-28 08:10:45 -0500

    reasons added to manager

--- cap_proj/src/Manager/mEQ.js
+++ cap_proj/src/Manager/mEQ.js
@@ -27,6 +27,7 @@ function MangerEQ(){
               <td className="data">{team[i].ptype}</td>
               <td className="data">{team[i].start_date.slice(0,10)}</td>
               <td className="data">{team[i].end_date.slice(0,10)}</td>
+              <td className="data">{team[i].reasons}</td>
               <td className="data">{team[i].status}</td>
           </tr>
       )
@@ -42,6 +43,7 @@ function MangerEQ(){
                       <th className="head">PTO Type</th>
                       <th className="head">Start</th>
                       <th className="head">End</th>
+                      <th className="head">Reason</th>
                       <th className="head">Status</th>
 
                   </tr>
